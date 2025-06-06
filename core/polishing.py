ebf61ed81205f6f08f694de617e11f538a296ebc
yfan <fanyucai3@gmail.com>
2025-06-06 14:50:12 +0800

    no

--- core/polishing.py
+++ core/polishing.py
@@ -11,7 +11,7 @@ def run(basecalls,DRAFT,outdir):
     cmd=(f'docker run --rm -v {DRAFT}:/raw_data/{DRAFT.split("/")[-1]} '
          f'-v {basecalls}:/raw_data/{basecalls.split("/")[-1]} '
          f'-v {os.path.abspath(outdir)}:/outdir/ '
-         f'{docker} sh -c \'export PATH=/opt/conda/bin:$PATH && '
+         f'{docker} sh -c \'export PATH=/opt/conda/envs/medaka/bin:$PATH && '
          f'medaka_consensus -i /raw_data/{basecalls.split("/")[-1]}'
          f' -d /raw_data/{DRAFT.split("/")[-1]} -o /outdir/ -t 24\'')
     subprocess.check_call(cmd,shell=True)
